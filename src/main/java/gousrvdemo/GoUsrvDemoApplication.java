fbcbdac8cb166ab2bdc79472c8adc0f53dda638f
glenn opdycke-hansen <glennoph@gmail.com>
2022-03-20 10:00:50 -0500

    init

---
+++ src/main/java/gousrvdemo/GoUsrvDemoApplication.java
@@ -0,0 +1,13 @@
+package gousrvdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoUsrvDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoUsrvDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gousrvdemo/GoUsrvDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package gousrvdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoUsrvDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
